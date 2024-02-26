b9009bf5d973b9e6ac56fb77fc261782a935d5d1
Adao-Angelo <adaobegginer@gmail.com>
2024-02-26 22:49:20 +0100

    config apperror end express.async.errr

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-import Express from "express";
+import Express, { NextFunction, Request, Response } from "express";
 import { router } from "./routers/index.router";
-
+import "express-async-errors";
+import { AppError } from "./errors/appErrors";
 const app = Express();
 const port = 2000;
 app.use(Express.json());
 app.use(router);
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server Error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server running at port: ${port}`);
