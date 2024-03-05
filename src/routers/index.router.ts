aa978cd42629e5ff2c4f23974298fa70d11ba858
Adao-Angelo <adaobegginer@gmail.com>
2024-03-05 13:23:03 +0100

    config auth

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -1,9 +1,12 @@
 import Router from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./../swagger.json";
 import { userRouter } from "./user.router";
 import { taskRouter } from "./Task.router";
 import { autheticationRouter } from "./auth.router";
 const router = Router();
 
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use("/users", userRouter);
 router.use("/tasks", taskRouter);
 router.use("/auth", autheticationRouter);
